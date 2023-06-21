8f065be11bf0de8e74f1ce0393ad28e2a12f144e
dosepehr <dosepehr@gmail.com>
2023-06-22 01:01:40 +0330

    add cartItems Qty to Navbar badge

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import CustomNumeralNumericFormat from './Price';
+import { useSelector } from 'react-redux';
 const Navbar = () => {
+    const cartItems = useSelector((state) => state.cart.cartItems);
     return (
         <header className='border-b border-palette-lighter sticky top-0 z-20 bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-6xl px-6 pb-2 pt-4 md:pt-6'>
@@ -17,13 +19,14 @@ const Navbar = () => {
                             className='fa fa-cart-arrow-down text-palette-primary m-auto'
                             style={{ fontSize: '30px' }}
                         ></i>
-                        {/* {cartItems.length === 0 ? null : ( */}
+                        {cartItems.length === 0 ? null : (
                             <div className='absolute top-0 right-0 text-xs bg-yellow-300 text-gray-900 font-semibold rounded-full py-1 px-2 transform translate-x-10 -translate-y-3'>
                                 <CustomNumeralNumericFormat
-                                    value={10}
+                                    value={cartItems.length}
                                     thousandSeparator=','
                                 />
                             </div>
+                        )}
                     </Link>
                 </div>
             </div>
